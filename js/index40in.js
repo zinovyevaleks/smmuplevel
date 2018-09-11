fb91053bc457601a89df7a421103297c1a690b5c
serega40in <serega40-in@yandex.ru>
2018-09-11 23:19:24 +0300

    Обыграл Охлобыстина

---
+++ js/index40in.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    PopUpHide1();
+    PopUpHide2();
+    PopUpHide3();
+    PopUpHide4();
+    PopUpHide5();
+});
+function PopUpShow1(){	
+    $("#tariff_popup1").show();
+}
+function PopUpHide1(){
+    $("#tariff_popup1").hide();
+}
+function PopUpShow2(){
+    $("#tariff_popup2").show();
+}
+function PopUpHide2(){
+    $("#tariff_popup2").hide();
+}
+function PopUpShow3(){
+    $("#tariff_popup3").show();
+}
+function PopUpHide3(){
+    $("#tariff_popup3").hide();
+}
+function PopUpShow4(){
+    $("#tariff_popup4").show();
+}
+function PopUpHide4(){
+    $("#tariff_popup4").hide();
+}
+function PopUpShow5(){	
+    $("#tariff_popup5").show();    
+}
+function PopUpHide5(){
+    $("#tariff_popup5").hide();
+}
